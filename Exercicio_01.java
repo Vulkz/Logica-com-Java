254c0d0d4aac1263a9e004080910d4b6283ad42d
Vulkz <gabrielcosta3536@gmail.com>
2023-03-10 00:02:35 -0300

    teste

---
+++ Exercicio_01.java
@@ -0,0 +1,5 @@
+public class Exercicio_01 {
+    public static void main(String args[]){
+        System.out.println("Hello, world!");
+    }
+}
---
+++ out/production/Logica-com-Java/README.md
@@ -0,0 +1,2 @@
+# Lógica-com-Java
+Esse repositório contem os exercicios de lógica da faculdade que fiz com java 
