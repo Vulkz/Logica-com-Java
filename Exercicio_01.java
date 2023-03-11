914230f3c3dd72edb77dbe4ea16348a7a32c7666
Vulkz <gabrielcosta3536@gmail.com>
2023-03-10 23:18:34 -0300

    add 1º Exercise

--- Exercicio_01.java
+++ Exercicio_01.java
@@ -1,5 +1,20 @@
+import javax.swing.JOptionPane;
 public class Exercicio_01 {
     public static void main(String args[]){
-        System.out.println("Hello, world!");
+        String nome;
+        int numNota=0;
+        double nota=0, media=0;
+
+        nome = JOptionPane.showInputDialog(null, "Nome do aluno: ", "Aluno", JOptionPane.INFORMATION_MESSAGE);
+
+        while(numNota<3){
+            nota = Double.parseDouble(JOptionPane.showInputDialog(null, (numNota+1)+"º Nota: ", "Nota", JOptionPane.INFORMATION_MESSAGE));
+            media = media + nota;
+            numNota++;
+        }
+
+        media = media / numNota;
+
+        JOptionPane.showMessageDialog(null, nome + " ficou com: " + media, "Média", JOptionPane.INFORMATION_MESSAGE);
     }
 }
