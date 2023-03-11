9451e642a145a97138586583ecf1fbc88832e61d
Vulkz <gabrielcosta3536@gmail.com>
2023-03-10 23:38:09 -0300

    add 2º exercise

---
+++ Exercicio_02.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio_02
+{
+    public static void main(String args[])
+    {
+        int idade=0, idadeDias=0;
+
+        idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Idade: ", "Anos", JOptionPane.INFORMATION_MESSAGE));
+
+        idadeDias = idade * 365;
+
+        JOptionPane.showMessageDialog(null, "Você viveu " + idadeDias + " dias", "Dias", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
