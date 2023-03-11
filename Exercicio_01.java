a67127233535d673ee94747242af389c3e5e6534
Vulkz <gabrielcosta3536@gmail.com>
2023-03-10 23:24:45 -0300

    1st adjusted exercise

--- Exercicio_01.java
+++ Exercicio_01.java
@@ -1,13 +1,17 @@
 import javax.swing.JOptionPane;
-public class Exercicio_01 {
-    public static void main(String args[]){
+
+public class Exercicio_01
+{
+    public static void main(String args[])
+    {
         String nome;
         int numNota=0;
         double nota=0, media=0;
 
         nome = JOptionPane.showInputDialog(null, "Nome do aluno: ", "Aluno", JOptionPane.INFORMATION_MESSAGE);
 
-        while(numNota<3){
+        while(numNota<3)
+        {
             nota = Double.parseDouble(JOptionPane.showInputDialog(null, (numNota+1)+"º Nota: ", "Nota", JOptionPane.INFORMATION_MESSAGE));
             media = media + nota;
             numNota++;
