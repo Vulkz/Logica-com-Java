d9cd7ac9eba458aed77d989b4041a3d97ea15f51
Vulkz <gabrielcosta3536@gmail.com>
2023-03-10 23:50:41 -0300

    add 3º Exercise

---
+++ Exercicio_03.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio_03
+{
+    public static void main(String args[])
+    {
+        String nome;
+
+        nome = JOptionPane.showInputDialog(null, "Nome: ", "Nome", JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Hy "+ nome + "!", "Saudação", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
