655596d72daebe38bb1056e0f19887c7eb56be99
Vulkz <gabrielcosta3536@gmail.com>
2023-03-11 00:06:00 -0300

    add 4º Exercise

---
+++ Exercicio_04.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio_04
+{
+    public static void main(String args[])
+    {
+        double grausCels, grausFahr;
+
+        grausCels = Double.parseDouble(JOptionPane.showInputDialog(null, "Graus Celsius: ", "Temperatura", JOptionPane.INFORMATION_MESSAGE));
+        grausFahr = (9 * grausCels + 160) / 5;
+
+        JOptionPane.showMessageDialog(null, "Graus fahrenheit: " + grausFahr, "Temperatura", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
